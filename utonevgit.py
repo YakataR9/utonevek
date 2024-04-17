7d1d81b7b1540b822b7bf413d943168e508ab30a
YakataR9 <116081382+YakataR9@users.noreply.github.com>
2024-04-17 08:03:44 +0200

    Házi feladat

--- utonevgit.py
+++ utonevgit.py
@@ -1,42 +1,67 @@
-class Nev:
-    def __init__(self,nev,elso,masodik,ujsz1,ujsz2,nem):
+class Utonev:
+    def __init__(self, nev, elso, masodik, uj1, uj2, nem):
         self.nev = nev
         self.elso = elso
         self.masodik = masodik
-        self.ujsz1 = ujsz1
-        self.ujsz2 = ujsz2
+        self.uj1 = uj1
+        self.uj2 = uj2
         self.nem = nem
+
     def __str__(self):
         return f"Utónév: {self.nev}, neme: {self.nem}"
 
+db = 0
 utonevek = []
+f = open("utonev.txt", "rt", encoding="ansi")
+f.readline()
 
-with open("utonevek/UTONEV.TXT", "rt", encoding="ansi") as f:
-    f.readline()
-    for sor in f:
-        sor = sor.strip().split(";")
-        utonevek.append(Nev(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5]))
-        
+for sor in f:
+    sor = sor.strip().split(';')
+    print(sor)
+    print(sor)
+    utonevek.append(Utonev(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5]))
+    db += 1
+
 for utonev in utonevek:
-    print(utonev) 
+    print(utonev)
 
-adat = 0        
-for utonev in utonevek:
-    adat += 1
-print(adat,"utónévről található adat a forrásban") 
+ffo = 0
+for ferfi in utonevek:
+    if ferfi.nem == 'F':
+        if ferfi.elso != '':
+            ffo += int(ferfi.elso)
+print(f"\n{ffo} férfi volt")
 
-elsok = 0
-for sor in utonevek:
-    if sor.nem == 'F':
-        if sor.elso != "":
-            elsok += int(sor.elso)
-print(elsok,"férfiról volt adat")
+nfo = 0
+for no in utonevek:
+    if no.nem == 'N':
+        if no.elso != '':
+            nfo += int(no.elso)
+print(f"{nfo} nő volt")
 
-elsokNo = 0
-for sor in utonevek:
-    if sor.nem == 'N':
-        if sor.elso != "":
-            elsokNo += int(sor.elso)
-print(elsokNo,"nőröl volt adat")
+print(f"\n{ffo + nfo} volt a népesség száma")
+print(f"{db} utónévről van adat") 
 
-print("Összesen",elsokNo + elsok,"emberről volt adat")
+ujfi = 0
+for fiu in utonevek:
+    if fiu.nem == 'F':
+        if fiu.uj1 != '':
+            ujfi += int(fiu.uj1)
+ujfi2 = 0
+for fiu2 in utonevek:
+    if fiu2.nem == 'F':
+        if fiu2.uj2 != '':
+            ujfi2 += int(fiu.uj2)
+print(f"\n{ujfi + ujfi2} újszülött fiú volt")
+
+ujla = 0
+for lany in utonevek:
+    if lany.nem == 'N':
+        if lany.uj1 != '':
+            ujla += int(lany.uj1)
+ujla2 = 0
+for lany2 in utonevek:
+    if lany2.nem == 'N':
+        if lany2.uj2 != '':
+            ujla2 += int(lany.uj2)
+print(f"{ujla + ujla2} újszülött lány volt\n")
